80961b989bdac390e5c3ad82d3cf4040b4bdf636
jkeller <jkeller@lnalaser.com>
2019-04-15 12:43:19 -0400

    Ensured conformity to 80 column limit, removed unused variables

--- MecaRobot.py
+++ MecaRobot.py
@@ -9,12 +9,9 @@ import socket
 class MecaRobot:
     """Robot class for programming Meca500 robots"""
     def __init__(self, ip, port):
-        self.BUFFER_SIZE = 512  # bytes
         self.TIMEOUT = 60       # seconds
         self.ip = ip
         self.port = port
-        #self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        #self.sock.settimeout(self.TIMEOUT)
         self.sock = None
         print('Connecting to robot %s:%i' % (ip, port))
         sys.stdout.flush()
@@ -27,7 +24,9 @@ class MecaRobot:
             if response[1:5] == "3000":
                 break
             elif time.perf_counter() - start_time >= self.TIMEOUT:
-                raise TimeoutError("Waited too long for the port {} on host {} to start accepting connections".format(port, ip))
+                raise TimeoutError("Timeout port {}, host {}".format(
+					port, ip)) from ex
+
             else:
                 time.sleep(0.5)
         
@@ -43,12 +42,14 @@ class MecaRobot:
         start_time = time.perf_counter()
         while True:
             try:
-                self.sock = socket.create_connection((self.ip, self.port), timeout=self.TIMEOUT)
+                self.sock = socket.create_connection(
+					(self.ip, self.port), timeout=self.TIMEOUT)
                 break
             except OSError as ex:
                 time.sleep(0.01)
                 if time.perf_counter() - start_time >= self.TIMEOUT:
-                    raise TimeoutError("Waited too long for the port {} on host {} to start accepting connections".format(port, ip)) from ex
+                    raise TimeoutError("Timeout port {}, host {}".format(
+						port, ip)) from ex
         
 
     def send_str(self, msg):
@@ -64,7 +65,8 @@ class MecaRobot:
         
     def run(self, cmd, values=None):
         if isinstance(values, list):
-            str_send = cmd + '(' + (','.join(format(vi, ".6f") for vi in values)) + ')'
+            str_send = cmd + '(' 
+				+ (','.join(format(vi, ".6f") for vi in values)) + ')'
         elif values is None:
             str_send = cmd
         else:
@@ -94,11 +96,12 @@ class MecaRobot:
         answer_timer = Timer(10, self.answer_not_found, args=error_message)
         robot_answer = ""
 
-        # loop to find answer, if answer is not found after 10 seconds print error message
+        #if answer is not found after 10 seconds print error message
         while robot_answer != answer:
             robot_output = self.receive_str()
 
-            if robot_output.find(answer) == -1 & answer_timer.is_alive() is False:
+            if robot_output.find(answer) == -1 
+				& answer_timer.is_alive() is False:
                 answer_timer.start()
                 answer_timer.join()
 
